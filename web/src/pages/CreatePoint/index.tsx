6ae6ddef978b01cf2e5259152303af0168fe045a
Mateus Leandro da Silva Vieira <mateus.lsvieira@dominio.local>
2020-06-05 16:15:06 -0300

    web continuos

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
+
+const CreatePoint = () => {
+  return(
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      <form action=""></form>
+    </div>
+  );
+};
+
+export default CreatePoint;
